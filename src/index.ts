267a0502145ecdbaeb18ad6787738bdb3a974718
Ylan Allouche <ylan@ylanallouche.xyz>
2026-01-13 15:16:04 -0500

    feat: add public API exports

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { SystematicCV, default } from "./systematic-cv.js";
+
+export { DataLoader } from "./core/data-loader.js";
+export { CVGenerator } from "./core/cv-generator.js";
+export { CVFilter } from "./core/filter.js";
+
+export { QueryBuilder } from "./builders/query-builder.js";
+
+export * from "./types/index.js";
